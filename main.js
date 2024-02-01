379506faf696449d7b5d9dfe61f4bc8c52073221
vohidovich5557 <shokhuk006@gmail.com>
2024-02-01 23:16:37 +0500

    slidernew

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
-$('.swiper-wrapper').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+
+var swiper = new Swiper(".card_slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        365: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 1,
+        },
+        1024: {
+            slidesPerView: 1,
+        },
+    },
+});
